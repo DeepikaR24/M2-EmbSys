d7ca2eeac77704d7117b0a0aafdef4405668dbe9
Deepika R <98866123+DeepikaR24@users.noreply.github.com>
2022-03-08 13:52:09 +0530

    Add files via upload

---
+++ project/3_Implementation/header file/out.h
@@ -0,0 +1,10 @@
+#ifndef OUT_H
+#define OUT_H
+#include <Arduino.h>
+
+void setupoutput(byte pin1,byte pin2);
+void runforward(byte pin1,byte pin2);
+void runbackward(byte pin1,byte pin2);
+void stop(byte pin1,byte pin2);
+
+#endif
