714586623cd71982148b082ad22604e84a7f6340
Deepika R <98866123+DeepikaR24@users.noreply.github.com>
2022-03-08 14:04:03 +0530

    Add files via upload

---
+++ project/3_Implementation/src file/out.cpp
@@ -0,0 +1,26 @@
+#include "out.h"
+
+void setupoutput(byte pin1,byte pin2)
+{
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT);
+}
+
+
+void runforward(byte pin1,byte pin2)
+{
+ digitalWrite(pin1, HIGH);   
+ digitalWrite(pin2, LOW);    
+}
+
+void runbackward(byte pin1,byte pin2)
+{
+ digitalWrite(pin2, HIGH);   
+ digitalWrite(pin1, LOW);    
+}
+
+void stop(byte pin1,byte pin2)
+{
+ digitalWrite(pin1, LOW);                        
+ digitalWrite(pin2, LOW);     
+}
