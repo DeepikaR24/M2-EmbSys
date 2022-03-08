d063314c63e1336ea1f664dcde036bb903d3e099
Deepika <deepi24kcm@gmail.com>
2022-03-08 22:49:54 +0530

    commit

--- project/3_Implementation/header file/door.h
+++ project/3_Implementation/header file/door.h
@@ -1,8 +1,8 @@
 #ifndef OUT_H
 #define OUT_H
-#include <Arduino.h>
+#include <arduino.h>
 
-void setupoutput(byte pin1,byte pin2);
+void setupoutput( int byte pin1,byte pin2);
 void runforward(byte pin1,byte pin2);
 void runbackward(byte pin1,byte pin2);
 void stop(byte pin1,byte pin2);
