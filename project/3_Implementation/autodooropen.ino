2497be8f0243df4f9e758341d9e212a4de55ce1f
Deepika R <98866123+DeepikaR24@users.noreply.github.com>
2022-03-08 13:59:27 +0530

    Add files via upload

---
+++ project/3_Implementation/autodooropen.ino
@@ -0,0 +1,30 @@
+#include "out.h"
+#define mpin1 8
+#define mpin2 9 
+const int pirPin = 3; 
+int pirState = 0;    
+
+void setup() {
+  setupoutput(mpin1,mpin2);
+    pinMode(pirPin, INPUT);
+
+}
+
+void loop() {
+
+  pirState = digitalRead(pirPin);
+
+  if (pirState == HIGH) 
+  {
+    runforward(mpin1,mpin2);
+    delay(2000);
+    stop(mpin1,mpin2);
+    delay(4000);
+    runbackward(mpin1,mpin2);
+    delay(2000);
+    stop(mpin1,mpin2);
+   
+  }
+    
+  
+}
